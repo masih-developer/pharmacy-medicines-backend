92cd6c5070d4e73f4b9871c1c0f3de2e06184842
masih abedini <masih7816@gmail.com>
2024-04-19 02:08:58 +0330

    ✨ (controllers & routes): add /file route for reading xlsx file

--- controllers/medicine.js
+++ controllers/medicine.js
@@ -1,4 +1,6 @@
 const MedicineModel = require("../models/medicine");
+const xlsx = require("xlsx");
+const { randomDate } = require("../utils");
 
 const createMedicine = async (req, res) => {
   const medicine = req.body;
@@ -11,4 +13,49 @@ const createMedicine = async (req, res) => {
   }
 };
 
-module.exports = { createMedicine };
+const readMedicineFromXlsx = async (req, res) => {
+  const fileBuffer = req.file.buffer;
+
+  try {
+    const workbook = xlsx.read(fileBuffer, { type: "buffer" });
+
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+
+    let data = xlsx.utils.sheet_to_json(sheet);
+
+    data = data.map((med) => {
+      return {
+        name: med["نام کالا"] || "",
+        expire: randomDate().toISOString() || "",
+        code: med["کد کالا"] || "",
+        quantity: med["کل موجودي"] !== undefined ? med["کل موجودي"] : "",
+        price: med["قيمت1"] || "",
+        type: med["واحد اصلي"] || "",
+      };
+    });
+
+    const results = [];
+
+    for (const record of data) {
+      try {
+        const medicine = new MedicineModel(record);
+
+        const savedMedicine = await medicine.save();
+
+        results.push(savedMedicine);
+      } catch (error) {
+        console.error("Error saving medicine record:", error);
+        results.push({ error: error.message, record });
+      }
+    }
+
+    res.json({ message: "success" });
+  } catch (error) {
+    // Handle any parsing errors
+    console.error("Error parsing XLSX file:", error);
+    res.status(500).send("Error parsing XLSX file");
+  }
+};
+
+module.exports = { createMedicine, readMedicineFromXlsx };
--- routes/medicine.js
+++ routes/medicine.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { createMedicine } = require("../controllers/medicine");
+const {
+  createMedicine,
+  readMedicineFromXlsx,
+} = require("../controllers/medicine");
+const multer = require("multer");
 
+const upload = multer();
 const router = express.Router();
 
 router.post("/", createMedicine);
+
+router.post("/file", upload.single("excel"), readMedicineFromXlsx);
 
 module.exports = router;
