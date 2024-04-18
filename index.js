0a4777671a4e6ef1d2a028ebfbfb9ec04b97e7cf
masih abedini <masih7816@gmail.com>
2024-04-18 18:18:35 +0330

    ♻️ (index.js): refactor connections

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-const server = app.listen(3000, () => {
-  console.log(`Server Runnded on port ${server.address().port}`);
-});
-
 app.get("/", (req, res) => {
   res.send("hello from app backend");
 });
 
 mongoose
   .connect("mongodb://127.0.0.1/pharmacy-medicines")
-  .then((e) => {
+  .then(() => {
     console.log("✅Connected To Db Successfully :)");
+    const server = app.listen(3000, () => {
+      console.log(`🚀Server Runnded on port ${server.address().port}`);
+    });
   })
   .catch(() => {
     console.log("❌Connected To Db Failed :)");
