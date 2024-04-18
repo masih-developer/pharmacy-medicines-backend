034eeeb882234a2fb381dca3f71df0054ebbcd63
masih abedini <masih7816@gmail.com>
2024-04-19 02:08:16 +0330

    🎨 (utils/index): add util fn for generate random expire date

---
+++ utils/index.js
@@ -0,0 +1,13 @@
+const randomDate = () => {
+  const minTime = Date.now();
+  const maxTime = Date.now() + 60 * 24 * 60 * 60 * 1000;
+
+  const randomTime =
+    Math.floor(Math.random() * (maxTime - minTime + 1)) + minTime;
+
+  const expirationDate = new Date(randomTime);
+
+  return expirationDate;
+};
+
+module.exports = { randomDate };
