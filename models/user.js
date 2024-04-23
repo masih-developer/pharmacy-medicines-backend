77bd0c5558cb62433bc248c6b3091408d1f4c3b4
masih abedini <masih7816@gmail.com>
2024-04-23 23:06:21 +0330

    ✨ (models/user): create user model

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = model("User", userSchema);
+
+module.exports = UserModel;
