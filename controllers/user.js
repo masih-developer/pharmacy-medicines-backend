aff236ec9d2051e5ab1e893aa19febbf75181f6a
masih abedini <masih7816@gmail.com>
2024-04-25 12:58:40 +0330

    🎨 (controller/user): create user controller

---
+++ controllers/user.js
@@ -0,0 +1,46 @@
+const UserModel = require("../models/user");
+const { registerSchema } = require("../validators/auth/index");
+const Yup = require("yup");
+const bcrypt = require("bcrypt");
+
+const registerUser = async (req, res, next) => {
+  try {
+    const { firstname, lastname, username, email, password } =
+      await registerSchema.validate(req.body, { abortEarly: false });
+
+    const isUserExist = await UserModel.find({
+      $or: [{ username }, { email }],
+    });
+
+    if (isUserExist) {
+      res.status(409).json({ message: "this user already registered" });
+      return;
+    }
+
+    const hashedPass = await bcrypt.hash(password, 12);
+
+    const user = await UserModel.create({
+      firstname,
+      lastname,
+      username,
+      email,
+      password: hashedPass,
+    });
+
+    res.json(user);
+  } catch (error) {
+    if (error instanceof Yup.ValidationError) {
+      res.status(422).json({ message: error.errors[0] });
+    }
+  }
+};
+
+const loginUser = (req, res) => {
+  res.json({ message: "user login successfully!" });
+};
+
+const getMeUser = (req, res) => {
+  res.json({ message: "get user information successfully!" });
+};
+
+module.exports = { registerUser, loginUser, getMeUser };
