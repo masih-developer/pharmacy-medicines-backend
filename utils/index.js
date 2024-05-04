cf73dc493ecf89a72f3b62eba3ac11f7c88e4230
masih abedini <masih7816@gmail.com>
2024-05-04 15:41:06 +0330

    ♻️ (utils/index): increase time of randomDate generator function

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,6 @@
 const randomDate = () => {
   const minTime = Date.now();
-  const maxTime = Date.now() + 60 * 24 * 60 * 60 * 1000;
+  const maxTime = Date.now() + 5 * 365 * 24 * 60 * 60 * 1000;
 
   const randomTime =
     Math.floor(Math.random() * (maxTime - minTime + 1)) + minTime;
