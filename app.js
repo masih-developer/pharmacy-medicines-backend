52bf2269f5485226b82d883b742a267d3c2c5e08
masih abedini <masih7816@gmail.com>
2024-05-11 20:20:47 +0330

    ♻️ (app.js): update cors policy

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(
   cors({
     credentials: true,
