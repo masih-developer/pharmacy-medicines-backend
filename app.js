3b04d3bb860d8355fe4ff709231463283e6e4ff3
masih abedini <masih7816@gmail.com>
2024-05-11 12:58:12 +0330

    ♻️ add some headers to "vercel.json"

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const medicineRoutes = require("./routes/medicine.routes");
 const userRoutes = require("./routes/user.routes");
 
 const app = express();
-console.log(process.env.ALLOW_CORS_ORIGIN.split("|"));
 
 app.use(
   cors({ credentials: true, origin: process.env.ALLOW_CORS_ORIGIN.split("|") })
